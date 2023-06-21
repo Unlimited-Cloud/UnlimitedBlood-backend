65ebe144babcef26ce15c5af5d0a1c206a69aab4
Swishhhhhh <chitrakarsuyash93@gmail.com>
2023-06-21 13:43:28 +0545

    official

---
+++ resources/views/post.blade.php
@@ -0,0 +1,16 @@
+<!doctvpe html>
+
+<title>My Bloobuhihihd</title>
+
+<link rel="stylesheet" href="/app.css">
+
+
+<body>
+
+<article>
+    <?= $post; ?>
+</article>
+
+<a href="/">GoBack</a>
+</body>
+
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,38 @@
+<!doctvpe html>
+
+<title>My Blood</title>
+
+<link rel="stylesheet" href="/app.css">
+
+<body>
+
+<article>
+    <h1><a href="/posts/my-first-post"> My First Post</a></h1>
+
+    <p>Blood is a body fluid in the circulatory system of humans and other vertebrates that delivers necessary
+        substances such as nutrients and oxygen to the cells, and transports metabolic waste products away from those
+        same cells.[1] Blood in the circulatory system is also known as peripheral blood, and the blood cells it
+        carries, peripheral blood cells.</p>
+</article>
+
+<article>
+
+    <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+    <p>Blood is a body fluid in the circulatory system of humans and other vertebrates that delivers necessary
+        substances such as nutrients and oxygen to the cells, and transports metabolic waste products away from those
+        same cells.[1] Blood in the circulatory system is also known as peripheral blood, and the blood cells it
+        carries, peripheral blood cells.</p>
+</article>
+
+<article>
+
+    <h1><a href="/posts/my-third-post"> My Third Post</a></h1>
+    <p>Blood is a body fluid in the circulatory system of humans and other vertebrates that delivers necessary
+        substances such as nutrients and oxygen to the cells, and transports metabolic waste products away from those
+        same cells.[1] Blood in the circulatory system is also known as peripheral blood, and the blood cells it
+        carries, peripheral blood cells.</p>
+</article>
+
+
+</body>
+

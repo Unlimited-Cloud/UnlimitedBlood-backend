8503d40a4fd144a90ed5b1f713017623e38e2e8e
sidtuladhar <smt9598@nyu.edu>
2023-06-23 12:26:38 +0545

    added logout API

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -25,11 +26,30 @@ class UserController extends Controller
                 return response()->json(['error' => 'Wrong password'], 401);
             }
 
-            //$user->loginStatus = true;
-            //$user->save();
+            $user->loginStatus = true;
+            $user->save();
 
             return response()->json(['success' => true, 'name' => $user['name'], 'bloodType' => $user['bloodType'], 'birthDate' => $user['birthDate'], 'email' => $user['email']]);
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
+            return response()->json(['error' => 'An error occurred', 'exception' => $e], 500);
+        }
+    }
+
+    public function logout(Request $request): JsonResponse
+    {
+        $phoneNumber = $request->input('phoneNumber');
+        try {
+            $user = User::where('phoneNumber', $phoneNumber)->first();
+
+            if (!$user) {
+                return response()->json(['error' => 'User not found'], 404);
+            }
+
+            $user->loginStatus = false;
+            $user->save();
+
+            return response()->json(['success' => true]);
+        } catch (Exception) {
             return response()->json(['error' => 'An error occurred'], 500);
         }
     }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
             $table->string('phoneNumber')->unique();
             $table->string('email')->unique();
             $table->string('name');
